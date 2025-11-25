d4d33d1177450d07f31a480c8c279cd4c48b3559
Yash Hegde <yash4success@gmail.com>
2025-11-25 18:44:32 +0530

    usescroll

--- src/pages/MotionHooksPage.tsx
+++ src/pages/MotionHooksPage.tsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import MotionHooks from "../components/MotionHooks";
+import { useMotionValueEvent, useScroll, motion } from "motion/react";
 
 function MotionHooksPage() {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const { scrollYProgress } = useScroll({
+    target: containerRef,
+    offset: ["start end", "end start"],
+  });
+
+  const backgrounds = ["#0f0f0f", "#1e1e2f", "#16212f"];
+  const [background, setBackground] = useState(backgrounds[0]);
+  useMotionValueEvent(scrollYProgress, "change", (latest) => {
+    setBackground(backgrounds[Math.floor(latest * backgrounds.length)]);
+  });
   return (
-    <div className="flex min-h-screen justify-center items-center bg-neutral-900">
+    <motion.div
+      ref={containerRef}
+      animate={{
+        background: background,
+      }}
+      transition={{
+        duration: 1,
+        ease: "easeInOut",
+      }}
+      className="flex min-h-screen justify-center items-center bg-neutral-900"
+    >
       <MotionHooks />
-    </div>
+    </motion.div>
   );
 }
 
