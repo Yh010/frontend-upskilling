46ccf5b05a1a13c3f569a74c90db2dfbfd15ebf3
Yash Hegde <yash4success@gmail.com>
2026-01-07 06:59:20 +0530

    switch statement for showing content based on content value

--- src/components/LandingPageComponents/MainBody.tsx
+++ src/components/LandingPageComponents/MainBody.tsx
@@ -3,7 +3,41 @@ import { ContentContext } from "../../contentContext.js";
 
 const MainBody = () => {
   const content = useContext(ContentContext);
-  return <div className="">context value is {content}</div>;
+
+  let output;
+  switch (content) {
+    case "home":
+      output = <p>home</p>;
+      break;
+    case "experience":
+      output = <p> experience</p>;
+      break;
+    case "motion":
+      output = <p> motion</p>;
+      break;
+
+    case "contactme":
+      output = <p> contactme</p>;
+      break;
+
+    case "projects":
+      output = <p> projects</p>;
+      break;
+
+    case "certificates":
+      output = <p> certificates</p>;
+      break;
+
+    case "blogsNmedia":
+      output = <p> blogsNmedia</p>;
+      break;
+
+    default:
+      output = <p>home</p>;
+      break;
+  }
+
+  return <div className="">{output}</div>;
 };
 
 export default MainBody;
