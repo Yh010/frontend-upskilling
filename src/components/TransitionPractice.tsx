46037f6ea9bfb4d8f15e445ef1c6a6ad7918c354
Yash Hegde <yash.hegde@oracle.com>
2026-02-04 15:43:12 +0530

    simple hover animation: move ball up by 20% on hover

--- src/components/TransitionPractice.tsx
+++ src/components/TransitionPractice.tsx
@@ -1,35 +1,9 @@
-import { TrashIcon } from "lucide-react";
 import { useEffect, useState, type Dispatch, type SetStateAction } from "react";
 
-interface CardProps {
-  idx: number;
-  selectedItems: number[];
-  setSelectedItems: Dispatch<SetStateAction<number[]>>;
-}
-const Card = ({ idx, selectedItems, setSelectedItems }: CardProps) => {
-  return (
-    <div
-      className="w-full h-10 rounded-xl bg-[#f5f3f4] text-[#252422] shadow-xs font-semibold
-                  flex justify-center items-center border border-[#d3d3d3] relative"
-    >
-      <span className="absolute">New Toast Created</span>
-      <input
-        type="checkbox"
-        className="absolute top-1 right-1 accent-red-500"
-        checked={selectedItems.includes(idx)}
-        onChange={(e) => {
-          setSelectedItems((prev) =>
-            e.target.checked ? [...prev, idx] : prev.filter((id) => id !== idx),
-          );
-        }}
-      ></input>
-    </div>
-  );
-};
 const Hover = () => {
   return (
-    <div>
-      hover
+    <div className="h-12 w-12 rounded-full bg-[#ffc300] hover:translate-y-[-20%] transition duration-300 ease-in-out">
+      
     </div>
   )
 }
@@ -58,25 +32,24 @@ const Toast = () => {
 }
 
 const TransitionPractice = () => {
-  const [selectedItems, setSelectedItems] = useState<number[]>([]);
-  const [confirmDelete, setConfirmDelete] = useState<Boolean>(false);
-  const [showOnlySelected, setShowOnlySelected] = useState<boolean>(false);
   const [transitionType, setTransitionType] = useState<
     "hover" | "CardHover" | "DownloadArrow" | "Toast"
   >("hover");
 
-  const cards = [1, 2, 3, 4];
-
-  const visibleCards = showOnlySelected
-    ? cards.filter((id) => selectedItems.includes(id))
-    : cards;
   const transitions = {
     hover: <Hover />,
     CardHover: <CardHover />,
     DownloadArrow: <DownloadArrow />,
     Toast : <Toast/>
-    
   };
+
+  const transitionDescriptions = {
+    hover: "The end goal is to move the ball 20% upwards on hover.",
+    CardHover: <CardHover />,
+    DownloadArrow: <DownloadArrow />,
+    Toast : <Toast/>
+  };
+
 
 
 
@@ -110,6 +83,9 @@ const TransitionPractice = () => {
       </div>
       <div className="border h-100 w-1/2 flex justify-center items-center rounded-3xl border-[#b1a7a6] shadow-xs">
         {transitions[transitionType] ?? null}
+      </div>
+      <div className="w-1/2 text-center text-[#66666e]">
+         {transitionDescriptions[transitionType] ?? null}
       </div>
     </div>
   );
